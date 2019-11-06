02bab62584bb9d1feb5ed7f1e3434687a779d484
Nathan Wisnoski <wisnoski@indiana.edu>
2019-11-06 15:14:35 -0500

    updated gcc version to use

--- mothur/hja-synoptic.sh
+++ mothur/hja-synoptic.sh
@@ -5,5 +5,5 @@
 #PBS -m abe
 #PBS -j oe
 cd /N/dc2/projects/Lennon_Sequences/hja-synoptic
-module load gcc/6.3.0
+module load gcc/9.1.0
 /N/u/wisnoski/Carbonate/mothur/mothur /gpfs/home/w/i/wisnoski/Carbonate/GitHub/hja-synoptic/mothur/hja-synoptic.batch
