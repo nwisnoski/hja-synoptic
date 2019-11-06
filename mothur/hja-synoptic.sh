5c1858b1aa80f0d14283e2dd959c4a8470e84d8c
Nathan Wisnoski <wisnoski@indiana.edu>
2019-11-06 15:12:31 -0500

    created mothur batch files

---
+++ mothur/hja-synoptic.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#PBS -k o
+#PBS -l nodes=1:ppn=8,vmem=500gb,walltime=240:00:00
+#PBS -M wisnoski@indiana.edu
+#PBS -m abe
+#PBS -j oe
+cd /N/dc2/projects/Lennon_Sequences/hja-synoptic
+module load gcc/6.3.0
+/N/u/wisnoski/Carbonate/mothur/mothur /gpfs/home/w/i/wisnoski/Carbonate/GitHub/hja-synoptic/mothur/hja-synoptic.batch
